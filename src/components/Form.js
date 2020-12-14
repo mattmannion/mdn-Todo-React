ae9049d31b888e9345f78be3b94db446aa43fd13
Matt Mannion <mattpmannion@gmail.com>
2020-12-14 18:47:17 -0500

    completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,6 +15,7 @@ function Form(props) {
 
     return (
         <form onSubmit={handleSubmit}>
+            <h1 className="heading-main ">TodoMatic</h1>
             <h2 className="label-wrapper">
                 <label htmlFor="new-todo-input" className="label__lg">
                     What needs to be done?
